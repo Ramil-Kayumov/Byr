635d6bbf3d8bc3b82f84d30ab2760faee466df10
Ramil-Kayumov <Kenny241105@gmail.com>
2024-10-30 09:45:05 +0300

    баг фикс

--- src/components/modal/register.jsx
+++ src/components/modal/register.jsx
@@ -7,78 +7,111 @@ import password from '../../assets/icons/password.png'; // Иконка для �
 const Register = ({ isOpen, onClose, onSwitchToAuth }) => {
   return (
     <AnimatePresence>
-      {isOpen && (
+    {isOpen && (
+      <motion.div
+        className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0, transition: { duration: 0.3 } }}
+      >
         <motion.div
-          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0, transition: { duration: 0.3 } }}
+          className="bg-[#31302E] h-[650px] w-[90%] lg:w-[600px] mx-auto border-2 border-black rounded-lg shadow-md pt relative"
+          initial={{ scale: 0.8 }}
+          animate={{ scale: 1 }}
+          exit={{ scale: 0.8, transition: { duration: 0.3 } }}
         >
-          <motion.div
-            className="bg-[#31302E] h-[650px] w-[600px] mx-auto border-2 border-black rounded-lg shadow-md pt relative"
-            initial={{ scale: 0.8 }}
-            animate={{ scale: 1 }}
-            exit={{ scale: 0.8, transition: { duration: 0.3 } }}
-          >
-            <a onClick={onClose}>
-              <motion.img src={close} alt="" whileHover={{scale:1.2}} className='cursor-pointer absolute right-6 top-8' />
-            </a>
-            <div className="flex flex-col items-center text-white mt-[53px]">
-              <h2 className='text-[40px] font-semibold'>Зарегистрироваться</h2>
-              
-              {/* Поле для имени с иконкой */}
-              <div className="relative mt-[50px]">
-                <input 
-                  type="text" 
-                  placeholder='Имя' 
-                  className='placeholder:text-white px-8 w-[487px] h-[60px] rounded-[20px] bg-[#615858] pr-12' 
-                />
-                <img src={user_icon} alt="user icon" className="absolute right-4 top-1/2 transform -translate-y-1/2 w-4 h-[18px]" />
-              </div>
-
-              {/* Поле для email с иконкой */}
-              <div className="relative mt-6">
-                <input 
-                  type="email" 
-                  placeholder='E-mail' 
-                  className='placeholder:text-white px-8 w-[487px] h-[60px] rounded-[20px] bg-[#615858] pr-12' 
-                />
-                <img src={email} alt="email icon" className="absolute right-4 top-1/2 transform -translate-y-1/2 w-5 h-4" />
-              </div>
-
-              {/* Поле для пароля с иконкой */}
-              <div className="relative mt-6">
-                <input 
-                  type="password" 
-                  placeholder='Пароль' 
-                  className='placeholder:text-white px-8 w-[487px] h-[60px] rounded-[20px] bg-[#615858] pr-12' 
-                />
-                <img src={password} alt="password icon" className="absolute right-4 top-1/2 transform -translate-y-1/2 w-[18px] h-[20px]" />
-              </div>
-
-              <button onClick={onSwitchToAuth} className='mt-[60px] w-[347px] h-[72px] rounded-[20px] bg-[#F7A539] text-2xl font-semibold'>Зарегистрироваться</button>
-              <p className='font-semibold text-2xl mt-6'>
-                У вас <motion.span initial={{ background: "transparent", color: "#74A0F5" }}
-                  whileHover={{
-                    background: [
-                      "linear-gradient(90deg, #F7A539, #74A0F5)",
-                      "linear-gradient(90deg, #74A0F5, #F7A539)",
-                      "linear-gradient(90deg, #F7A539, #74A0F5)",
-                    ],
-                    WebkitBackgroundClip: "text",
-                    color: "transparent",
-                    transition: {
-                      duration: 2,
-                      ease: "easeInOut",
-                      repeat: Infinity,
-                    },
-                  }} onClick={onSwitchToAuth} className='text-[#74A0F5] cursor-pointer'>есть учетная запись</motion.span> ?
-              </p>
+          <a onClick={onClose}>
+            <motion.img
+              src={close}
+              alt=""
+              className="cursor-pointer absolute right-6 top-8"
+              whileHover={{ scale: 1.2 }}
+            />
+          </a>
+          <div className="flex flex-col items-center text-white mt-[53px]">
+            <h2 className="text-[28px] md:text-[40px] font-semibold">
+              Зарегистрироваться
+            </h2>
+  
+            {/* Поле для имени с иконкой */}
+            <div className="relative mt-[30px] md:mt-[50px] w-[90%] md:w-[487px]">
+              <input
+                type="text"
+                placeholder="Имя"
+                className="placeholder:text-white px-4 md:px-8 w-full h-[60px] rounded-[20px] bg-[#615858] pr-12"
+              />
+              <img
+                src={user_icon}
+                alt="user icon"
+                className="absolute right-4 top-1/2 transform -translate-y-1/2 w-4 h-[18px]"
+              />
             </div>
-          </motion.div>
+  
+            {/* Поле для email с иконкой */}
+            <div className="relative mt-6 w-[90%] md:w-[487px]">
+              <input
+                type="email"
+                placeholder="E-mail"
+                className="placeholder:text-white px-4 md:px-8 w-full h-[60px] rounded-[20px] bg-[#615858] pr-12"
+              />
+              <img
+                src={email}
+                alt="email icon"
+                className="absolute right-4 top-1/2 transform -translate-y-1/2 w-5 h-4"
+              />
+            </div>
+  
+            {/* Поле для пароля с иконкой */}
+            <div className="relative mt-6 w-[90%] md:w-[487px]">
+              <input
+                type="password"
+                placeholder="Пароль"
+                className="placeholder:text-white px-4 md:px-8 w-full h-[60px] rounded-[20px] bg-[#615858] pr-12"
+              />
+              <img
+                src={password}
+                alt="password icon"
+                className="absolute right-4 top-1/2 transform -translate-y-1/2 w-[18px] h-[20px]"
+              />
+            </div>
+  
+            <button
+              onClick={onSwitchToAuth}
+              className="mt-[30px] md:mt-[60px] w-[90%] md:w-[347px] h-[72px] rounded-[20px] bg-[#F7A539] text-2xl font-semibold"
+            >
+              Зарегистрироваться
+            </button>
+            <p className="font-semibold text-2xl mt-6 text-center">
+              У вас{" "}
+              <motion.span
+                className="text-[#74A0F5] cursor-pointer"
+                onClick={onSwitchToAuth}
+                initial={{ background: "transparent", color: "#74A0F5" }}
+                whileHover={{
+                  background: [
+                    "linear-gradient(90deg, #F7A539, #74A0F5)",
+                    "linear-gradient(90deg, #74A0F5, #F7A539)",
+                    "linear-gradient(90deg, #F7A539, #74A0F5)",
+                  ],
+                  WebkitBackgroundClip: "text",
+                  color: "transparent",
+                  transition: {
+                    duration: 2,
+                    ease: "easeInOut",
+                    repeat: Infinity,
+                  },
+                }}
+              >
+                есть учетная запись
+              </motion.span>{" "}
+              ?
+            </p>
+          </div>
         </motion.div>
-      )}
-    </AnimatePresence>
+      </motion.div>
+    )}
+  </AnimatePresence>
+  
   );
 };
 
